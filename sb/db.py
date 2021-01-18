a9d95f502b17815e3544b6565acf1244add7fbb2
jericoSalvador <jericoSalvador@noreply.github.com>
2021-01-17 22:43:06 -0800

    updated db commands

--- sb/db.py
+++ sb/db.py
@@ -1,7 +1,7 @@
 
-import sqlite3 
+import sqlite3
 
-class Database(): 
+class BoardDatabase(): 
 
     conn = None 
 
@@ -11,9 +11,11 @@ class Database():
             self.initializeTables()
             print("DB Initialized")
         except sqlite3.OperationalError as e: 
-            print(e)
-            print("Loaded DB ")
-        except Error as e: 
+            if 'already exists' in e.__str__():     
+                print("Loaded DB ")
+            else: 
+                print(e)
+        except Exception as e: 
             print(e)
     
     def __del__(self):
@@ -23,39 +25,97 @@ class Database():
         self.conn.execute("""
             CREATE TABLE Task 
             (taskid INTEGER PRIMARY KEY AUTOINCREMENT, 
-            title TEXT NOT NULL, 
+            title TEXT UNIQUE NOT NULL, 
             description TEXT NOT NULL);
         """)
         self.conn.execute("""
-            CREATE TABLE SPRINT(
+            CREATE TABLE Sprint(
                 sprintid INTEGER PRIMARY KEY AUTOINCREMENT,
                 sprintname TEXT NOT NULL, 
-                taskid, integer, 
+                taskid INTEGER,
+                status TEXT DEFAULT 'todo' CHECK(status = 'todo' OR status = 'ip' OR status = 'done'), 
+                UNIQUE(sprintname, taskid),
                 FOREIGN KEY(taskId) REFERENCES Task(taskid) ON DELETE CASCADE
-            );
+                ); 
         """)
 
     def addTask(self, taskName, taskDescription): 
-        self.conn.execute(
-            f"""
-            INSERT INTO Task(title,description) VALUES('{taskName}', '{taskDescription}');
+        try: 
+            self.conn.execute(
+                f"""
+                INSERT INTO Task(title,description) VALUES('{taskName}', '{taskDescription}');
+                """
+            )
+            self.conn.commit()
+            print("taskadded")
+        except sqlite3.IntegrityError as e: 
+            print("Task already exists")
 
-            """
-        )
-        self.conn.commit()
-        print("taskadded")
-
-    def getTasks(self):
+    def getAllTasks(self):
         cur = self.conn.cursor()
-        cur.execute("SELECT * FROM Task")
+        cur.execute("SELECT * FROM Task;")
 
         data = cur.fetchall()
-        for row in data: 
-            print(row)
+        return data
+
+    def getTask(self, taskname):
+        cur = self.conn.cursor()
+        cur.execute(f"SELECT taskid FROM Task WHERE title = '{taskname}';")
+        return cur.fetchall()
+
+    def getSprintTasks(self, sprintname): 
+        cur = self.conn.cursor()
+        cur.execute(f"""
+            SELECT Sprint.sprintname, Task.title, Task.Description, Sprint.status
+            FROM Sprint LEFT JOIN Task ON Sprint.taskid = Task.taskid
+            WHERE Sprint.sprintname = '{sprintname}'; 
+        """)
+
+        return cur.fetchall()
+
+    def getTaskId(self, taskname): 
+        cur = self.conn.cursor()
+        cur.execute(f"SELECT taskid FROM Task WHERE title = '{taskname}'")
+
+        return cur.fetchone()[0]
+
+    def addTaskToSprint(self, taskname, sprintname): 
+        try: 
+            self.conn.execute(
+            f"""
+                INSERT INTO Sprint(sprintname,taskid)
+                VALUES ('{sprintname}', {self.getTaskId(taskname)}); 
+            """)
+            self.conn.commit()
+        
+        except Exception as e: 
+            print(e) 
+
+    def removeTaskFromSprint(self, taskname, sprintname): 
+        taskid = self.getTaskId(taskname)
+        self.conn.execute(
+        f"""
+            DELETE FROM Sprint
+            WHERE taskid = {taskid} AND sprintname = '{sprintname}'; 
+        """)
+        self.conn.commit()
+
+        
+def main():
+    db = BoardDatabase()
+    db.addTask("task1", "description for task1" )
+    db.addTask("Task2", "description2")
+    db.addTask("task1", "trying to add another task1 should fail")
+    db.addTask("task3", "description3") 
+
+    for row in db.getAllTasks():
+        print(row)
+    print(db.getTaskId('task3'))
+    db.addTaskToSprint('Task2', 'sprint1')
+    print(db.getSprintTasks('sprint1'))
+    db.removeTaskFromSprint('Task2','sprint1')
+    print(db.getSprintTasks('sprint1'))
+
 
 if __name__ == "__main__": 
-    db = Database()
-    db.addTask("Task3", "description for task3")
-    db.addTask("Task2", "description for task2")
-    db.addTask("Task4", "description for task4")
-    db.getTasks()
+    main()
